f6bbb01afacf9936b8dc50376d6ca593aaaeb138
Shivank Goel <shivankgoel003@gmail.com>
2024-03-07 22:07:45 -0500

    cleaning update

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,55 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the two data sets for the years 2018 to 2021
+# Author: Navya Gupta, Shivank Goel, Vanshika Vanshika
+# Date: 07 March 2024 
+# Contact: shivankg.goel@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 #### Workspace setup ####
 library(tidyverse)
+library(xlsx)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+#death-by-gender-and-age data
+
+file_path <- "data/raw_data/deaths-by-gender-and-age.csv"
+# Extracting 2018 data from death-by-gender-and-age data
+death_age_gender_2018 <- read.csv(file_path, skip = 19319, nrows = 20538 - 19320)
+# Extracting 2019 data from death-by-gender-and-age data
+death_age_gender_2019 <- read.csv(file_path, skip = 20538, nrows = 21380 - 20539)
+# Extracting 2020 data from death-by-gender-and-age data
+death_age_gender_2020 <- read.csv(file_path, skip = 21380, nrows = 22656 - 21381)
+# Extracting 2021 data from death-by-gender-and-age data
+death_age_gender_2021 <- read.csv(file_path, skip = 22656, nrows = 23864 - 22657)
+# Extracting 2022 data from death-by-gender-and-age data
+death_age_gender_2022 <- read.csv(file_path, skip = 23864, nrows = 25176 - 23865)
+
+# death-leading-causes-data
+
+# extracting 2018 data from death-leading-causes-data
+death_causes_2018 = read.xlsx("data/raw_data/deaths-leading-causes.xlsx", "Sheet1", rowIndex = 516:545)
+# extracting 2019 data from death-by-gender-and-age data
+death_causes_2019 = read.xlsx("data/raw_data/deaths-leading-causes.xlsx", "Sheet1", rowIndex = 546:575)
+# extracting 2020 data from death-by-gender-and-age data
+death_causes_2020 = read.xlsx("data/raw_data/deaths-leading-causes.xlsx", "Sheet1", rowIndex = 576:605)
+# extracting 2021 data from death-by-gender-and-age data
+death_causes_2021 = read.xlsx("data/raw_data/deaths-leading-causes.xlsx", "Sheet1", rowIndex = 606:635)
+# extracting 2022 data from death-by-gender-and-age data
+death_causes_2022 = read.xlsx("data/raw_data/deaths-leading-causes.xlsx", "Sheet1", rowIndex = 637:666)
+
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(death_age_gender_2018, "data/analysis_data/death_cause_by_gender_age/2018.csv")
+write_csv(death_age_gender_2019, "data/analysis_data/death_cause_by_gender_age/2019.csv")
+write_csv(death_age_gender_2020, "data/analysis_data/death_cause_by_gender_age/2020.csv")
+write_csv(death_age_gender_2021, "data/analysis_data/death_cause_by_gender_age/2021.csv")
+write_csv(death_age_gender_2022, "data/analysis_data/death_cause_by_gender_age/2022.csv")
+
+write_csv(death_causes_2018, "data/analysis_data/death_causes/2018.csv")
+write_csv(death_causes_2019, "data/analysis_data/death_causes/2019.csv")
+write_csv(death_causes_2020, "data/analysis_data/death_causes/2020.csv")
+write_csv(death_causes_2021, "data/analysis_data/death_causes/2021.csv")
+write_csv(death_causes_2022, "data/analysis_data/death_causes/2022.csv")
+
